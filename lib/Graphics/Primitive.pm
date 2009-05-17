8a11cd539d464557ebeeda12f7e8e2e225664246
Cory Watson <github@onemogin.com>
2009-05-17 17:31:23 -0500

    Make layout_manager a required attr of Container

--- lib/Graphics/Primitive.pm
+++ lib/Graphics/Primitive.pm
@@ -1,7 +1,7 @@
 package Graphics::Primitive;
 use Moose;
 
-our $VERSION = '0.42';
+our $VERSION = '0.43';
 
 __PACKAGE__->meta->make_immutable;
 
--- lib/Graphics/Primitive/Container.pm
+++ lib/Graphics/Primitive/Container.pm
@@ -23,7 +23,8 @@ has 'layout_manager' => (
     is => 'rw',
     isa => 'Layout::Manager',
     handles => [ 'do_layout' ],
-    trigger => sub { my ($self) = @_; $self->prepared(0); }
+    trigger => sub { my ($self) = @_; $self->prepared(0); },
+    required => 1
 );
 
 sub add_component {
